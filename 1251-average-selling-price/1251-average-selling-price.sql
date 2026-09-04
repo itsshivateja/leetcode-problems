b44c2e81a31faa48ac84e4144e089d1f017c8e94
Shiva Teja <shivateja.kamatam@gmail.com>
2026-09-04 14:46:36 +0530

    Time: 990 ms (32.48%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-SELECT p.product_id, ROUND(IFNULL(SUM(p.price * us.units) / SUM(us.units), 0), 2)
+SELECT p.product_id, ROUND(IFNULL(SUM(price * units) / SUM(units), 0), 2)
 AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold us
